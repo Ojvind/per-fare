a2319e3b17eba23ce4b09ac8147d1617823928aa
Öjvind Otterbjörk <ojvind.otterbjork@icloud.com>
2023-08-04 19:01:03 +0200

    Initial commit

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TodoList from './TodoList.vue';
+import TodoForm from './TodoForm.vue';
+</script>
+
+<template>
+    <div class="todo-app">
+        <h1>To Do List</h1>
+        <TodoForm />
+        <TodoList />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Todo Form comp</h1>
+</template>
+
+<style scoped>
+
+</style>
