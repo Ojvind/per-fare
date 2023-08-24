9a63cf8af7185545e7c137c0408168a6ed43fd6e
Öjvind Otterbjörk <ojvind.otterbjork@icloud.com>
2023-08-24 16:37:22 +0200

    Delete from store

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,33 +1,42 @@
 <script setup>
-import { useTodoListStore } from '../stores/todoList'
-import { storeToRefs } from 'pinia'
+import { useTodoListStore } from "../stores/todoList";
+import { storeToRefs } from "pinia";
 
-const store = useTodoListStore()
+const store = useTodoListStore();
 
-const { todoList } = storeToRefs(store) 
-const { toggleCompleted } = store
-
+// storeToRefs lets todoList keep reactivity:
+const { todoList } = storeToRefs(store);
+// destructuring action method doesn't require using storeToRefs:
+const { toggleCompleted, deleteTodo } = store;
 </script>
 
 <template>
-    <div v-for="todo in todoList" :key="todo.id" class="item">
-        <div class="content">
-            <span :class="{ completed: todo.completed}">{{ todo.item }}</span>
-            <span class="buttonBorder" @click.stop="toggleCompleted(todo.id)">
-                &#10004;</span>
-        </div>
+  <div v-for="todo in todoList" :key="todo.id">
+    <div>
+      <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
+      <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+      <span @click="deleteTodo(todo.id)">&#10060;</span>
     </div>
+  </div>
 </template>
 
 <style scoped>
+span {
+  margin: 0 10px;
+  cursor: pointer;
+}
+.item {
+  display: flex;
+  justify-content: center;
+}
+.content {
+  display: flex;
+  font-size: 1.5em;
+  justify-content: space-between;
+  width: 80vw;
+  padding: 5px;
+}
 .completed {
-    text-decoration: line-through;
+  text-decoration: line-through;
 }
-.buttonBorder {
-    border: 1px solid black;
-    padding: 0 5px;
-    border-radius: 5px;
-    cursor: pointer;
-}
-
 </style>
