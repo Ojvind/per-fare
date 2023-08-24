979f460e671813e3a3bfe6c285e6aae8ad773a41
Öjvind Otterbjörk <ojvind.otterbjork@icloud.com>
2023-08-24 15:44:35 +0200

    Pinia Fundamentals

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,9 +1,27 @@
 <script setup>
+import { ref } from 'vue'
+import { useTodoListStore } from '@/stores/todoList'
+
+const todo = ref('')
+const store = useTodoListStore()
+
+function addItemAndClear(item) {
+    if (item.length === 0) {
+        return
+    }
+    store.addTodo(item)
+    todo.value = ''
+}
 
 </script>
 
 <template>
-    <h1>Todo Form comp</h1>
+    <div>
+        <form @submit.prevent="addItemAndClear(todo)">
+            <input v-model="todo" type="text" placeholder="Add a new todo">
+            <button>Add</button>
+        </form>
+    </div>
 </template>
 
 <style scoped>
--- src/stores/todoList.js
+++ src/stores/todoList.js
@@ -2,6 +2,23 @@ import { defineStore } from 'pinia';
 
 export const useTodoListStore = defineStore('todoList', {
     state: () => ({
-        todList: []
+        todoList: [],
+        id: 0
     }),
+   actions: {
+        addTodo(item) {
+            this.todoList.push({ item, id: this.id++, completed: false })
+        },
+        deleteTodo(itemId) {
+            this.todoList = this.todList.filter((object) => {
+                return object.id !== itemId
+            })
+        },
+        toggleCompleted(idToFind) {
+            const todo = this.todoList.find((object) => object.id === idToFind)
+            if (todo) {
+                todo.completed = !todo.completed
+            }
+        }   
+    }
 });
